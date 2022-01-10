8e673a40a5a415af4ebfbd32ab411f74ed8f9c7e
vinaykashyap1996 <vinay.kashyap234@gmail.com>
2022-01-10 19:25:49 +0100

    create a comments and likes

--- Graphql/Schema/typeDef.js
+++ Graphql/Schema/typeDef.js
@@ -6,6 +6,21 @@ module.exports = gql`
     body: String!
     username: String!
     createdAt: String!
+    comments: [Comment]!
+    likes: [Like]!
+    likeCount: Int!
+    commentCount: Int!
+  }
+  type Comment {
+    _id: ID!
+    body: String!
+    username: String!
+    createdAt: String!
+  }
+  type Like {
+    _id: ID!
+    username: String!
+    createdAt: String!
   }
 
   type User {
@@ -33,5 +48,11 @@ module.exports = gql`
     login(username: String!, password: String!): User!
     createPost(body: String!): Post!
     deletePost(postId: ID!): String!
+    createComment(postId: ID!, body: String!): Post!
+    deleteComment(postId: ID!, commentId: ID!): Post!
+    likePost(postId: ID!): Post!
+  }
+  type Subscription {
+    newPost: Post!
   }
 `;
